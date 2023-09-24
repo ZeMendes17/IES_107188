764ae8e5d829b68855fb63b4ed351bd6bb2caf7b
Ze Mendes <zepedrosmendes@gmail.com>
2023-09-24 13:38:10 +0100

    Added log feature with log4j2 from location2

--- lab1/README.md
+++ lab1/README.md
@@ -12,3 +12,19 @@ In order to compile and run the project:
 --> mvn package #get dependencies, compiles the project and creates the jar
 --> mvn exec:java -Dexec.mainClass="{DgroupId.MainFuncName}" #adapt to match your own package structure and class name
 Obs: If we want to add parameters use: -Dexec.args="{one or more args}"
+
+To write a log we can use Log4j2 in java.
+Add the dependencies to pom.xml:
+	<dependency>
+            <groupId>org.apache.logging.log4j</groupId>
+            <artifactId>log4j-api</artifactId>
+            <version>2.6.1</version>
+        </dependency>
+        <dependency>
+            <groupId>org.apache.logging.log4j</groupId>
+            <artifactId>log4j-core</artifactId>
+            <version>2.6.1</version>
+        </dependency>
+And create a file named log4j2.xml in the Maven resources folder.
+We can add log through file or console or even both. This is done by adding appenders in the
+xml file (File and Console) and then referencing them in loggers as root.
--- lab1/lab1_2/MyWeatherRadar/src/main/java/ua/pt/WeatherStarter.java
+++ lab1/lab1_2/MyWeatherRadar/src/main/java/ua/pt/WeatherStarter.java
@@ -6,6 +6,8 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import ua.pt.IpmaCityForecast; //may need to adapt package name
 import ua.pt.IpmaService;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 
 /**
  * demonstrates the use of the IPMA API for weather forecast
@@ -14,6 +16,7 @@ public class WeatherStarter {
 
     //todo: should generalize for a city passed as argument
     //private static final int CITY_ID_AVEIRO = 1010500;
+    private static Logger logger = LogManager.getLogger(WeatherStarter.class);
 
     public static void  main(String[] args ) {
 
@@ -25,8 +28,17 @@ public class WeatherStarter {
 
         // create a typed interface to use the remote API (a client)
         IpmaService service = retrofit.create(IpmaService.class);
+        int city_id = 0;
+        try {
+            city_id = Integer.parseInt(args[0]);
+            logger.info("City id given as argument: " + city_id);
+        } catch (NumberFormatException e) {
+            logger.error("Incorrect argument given, please give a valid city id as argument");
+            System.exit(1);
+        }
         // prepare the call to remote endpoint
-        Call<IpmaCityForecast> callSync = service.getForecastForACity(Integer.parseInt(args[0])); // passed by the user
+        logger.info("Preparing call to api");
+        Call<IpmaCityForecast> callSync = service.getForecastForACity(city_id); // passed by the user
 
         try {
             Response<IpmaCityForecast> apiResponse = callSync.execute();
@@ -35,6 +47,7 @@ public class WeatherStarter {
             if (forecast != null) {
                 var firstDay = forecast.getData().listIterator().next();
 
+                logger.info("Printing results for city: " + forecast.getGlobalIdLocal());
                 System.out.printf("Predictions for day: %s %n%nMaximum temperature is %4.1f.%nMinimum temperature is %4.1f %n" +
                                 "Percentage of precipitation is %s %nWind direction is %s %nWind speed is %s %n",
                         firstDay.getForecastDate(),
@@ -43,13 +56,14 @@ public class WeatherStarter {
                         firstDay.getPrecipitaProb(),
                         firstDay.getPredWindDir(),
                         firstDay.getClassWindSpeed()
-                        );
+                );
             } else {
+                logger.error("No results for this request!");
                 System.out.println( "No results for this request!");
             }
         } catch (Exception ex) {
             ex.printStackTrace();
         }
-
+        logger.info("Done.");
     }
 }
